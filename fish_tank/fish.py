86a5bfd6b5d4e0a085a35918f4dcf40c1c900a37
Andrew Cannan <andrewmcannan@gmail.com>
2024-07-13 15:17:45 +0100

    create three subclasses of fish

--- fish_tank/fish.py
+++ fish_tank/fish.py
@@ -8,3 +8,14 @@ class Fish(ABC):
     def food_required(self) -> float:
         pass
     
+class Goldfish(Fish):
+    def food_required(self) -> float:
+        return 0.1
+    
+class Angelfish(Fish):
+    def food_required(self) -> float:
+        return 0.2
+    
+class Babelfish(Fish):
+    def food_required(self) -> float:
+        return 0.3
